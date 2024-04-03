dde78adf7086ce57bfeb320192a4994ac05de678
AntonioVPerez <122927677+AntonioVPerez@users.noreply.github.com>
2024-04-03 02:26:50 +0000

    part 2

--- reactcatalogtailwind/src/Shopping.js
+++ reactcatalogtailwind/src/Shopping.js
@@ -5,17 +5,32 @@ const Shop = () => {
     const [cart, setCart] = useState([]);
     const [cartTotal, setCartTotal] = useState(0);
 
+    function howManyofThis(id) {
+      let hmot = cart.filter((cartItem) => cartItem.id === id);
+      return hmot.length;
+    }
 
     const listItems = items.map((el) => (
-      <div key={el.id}>
-        <img class="img-fluid" src={el.image} width={100} />
-        {el.title}
-        {el.category}
-        {el.price}
-        <button type="button" onClick={() => removeFromCart(el)}>-</button>{" "}
-        <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+      // PRODUCT
+      <div class="row border-top border-bottom" key={el.id}>
+      <div class="row main align-items-center">
+      <div class="col-2">
+      <img class="img-fluid" src={el.image} />
       </div>
-    ));
+      <div class="col">
+      <div class="row text-muted">{el.title}</div>
+      <div class="row">{el.category}</div>
+      </div>
+      <div class="col">
+      <button type="button" variant="light" onClick={() => removeFromCart(el)} > - </button>{" "}
+      <button type="button" variant="light" onClick={() => addToCart(el)}> + </button>
+      </div>
+      <div class="col">
+      ${el.price} <span class="close">&#10005;</span>{howManyofThis(el.id)}
+      </div>
+      </div>
+      </div>
+      ));
 
     const addToCart = (el) => {
         setCart([...cart, el]);
@@ -49,13 +64,35 @@ const Shop = () => {
 
     return (
       <div>
-        <div>{listItems}</div>
-        <div>Itesm in Cart :</div>
-        <div>{cartItems}</div>
-        <div>Order total to pay :{cartTotal}</div>
-      </div>
-    );
-        
-}
+        STORE SE/ComS319
+        <div class="card">
+          <div class="row">
+          {/* HERE, IT IS THE SHOPING CART */}
+          <div class="col-md-8 cart">
+          <div class="title">
+          <div class="row">
+          <div class="col">
+          <h4>
+          <b>319 Shopping Cart</b>
+          </h4>
+          </div>
+          <div class="col align-self-center text-right text-muted">
+          Products selected {cart.length}
+          </div>
+          </div>
+          </div>
+          <div>{listItems}</div>
+          </div>
+          <div class ="float-end">
+          <p class ="mb-0 me-5 d-flex align-items-center">
+          <span class ="small text-muted me-2">Order total:</span>
+          <span class ="lead fw-normal">${cartTotal}</span>
+          </p>
+          </div>
+          </div>
+          </div>
+        </div>
+        );
+      };
 
-export default Shop;
+      export default Shop;
